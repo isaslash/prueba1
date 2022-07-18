370dadc31ce406f8e778923385fb87e0af929a1d
isaslash <isaiaslizaso@gmail.com>
2022-07-18 16:16:39 -0300

    probando

---
+++ Assets/BirdBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdBehaviour : MonoBehaviour {
+	public float speed;
+	public float rotationSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		move ();
+	}
+	void move ()
+	{
+		if (Input.GetKey (KeyCode.A))
+			transform.Rotate (-Vector3.forward * speed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.D))
+			transform.Rotate (Vector3.forward * speed * Time.deltaTime);
+		if(Input.GetKey (KeyCode.W))
+			transform.position += transform.forward * speed * Time.deltaTime;
+			
+	}
+}
